feb56f8e0bc79fd95ff430dad3c42941b8f6770d
Dimitris <dimitris_li@hotmail.com>
2020-12-17 02:13:53 +0200

    merge react branch

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
             services.AddDbContext<DataContext>(opt=> {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -55,10 +60,14 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseCors("CorsPolicy");
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            // app.UseMvc();
         }
     }
 }
